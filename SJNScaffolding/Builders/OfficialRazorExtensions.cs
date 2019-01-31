12e1781c7e36797e9cd7f9c260c027040d796407
IGeekFan <710277267@qq.com>
2019-01-31 13:39:40 +0800

    update controler  to jsonresult

--- SJNScaffolding/Builders/OfficialRazorExtensions.cs
+++ SJNScaffolding/Builders/OfficialRazorExtensions.cs
@@ -12,10 +12,6 @@
   */
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SJNScaffolding.Builders
 {
